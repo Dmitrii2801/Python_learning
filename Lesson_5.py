7c7a6b1b488dbe490aa4b704c467e2f95d946ef2
Dmitrii2801 <dimaster2804@gmail.com>
2026-08-07 20:15:41 +0500

    Create Lesson_5.py

---
+++ Lesson_5.py
@@ -0,0 +1,8 @@
+# Конкатенация (сложение) строк
+
+language = input('Введите целевой язык программирования: ')
+profession = input('Введите занимаемую должность:')
+year = 2026;
+
+resume = language + profession + str(year);
+print(resume)
