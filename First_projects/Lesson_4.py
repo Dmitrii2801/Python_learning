90b78eb7738ddd4289dcf126d4456c1c8d203907
Dmitrii2801 <dimaster2804@gmail.com>
2026-08-10 17:45:19 +0500

    New lessons

---
+++ First_projects/Lesson_4.py
@@ -0,0 +1,19 @@
+# Определение и вывод типов данных
+
+a = 12; # целочисленный тип данных (целое число)
+b = 'слово'; # строковый тип данных (строка)
+c = False; # логический тип данных
+d = 1.25; # тип данных с плавающей точкой
+n = None; # тип данных None
+
+a_type = type(a);
+b_type = type(b);
+c_type = type(c);
+d_type = type(d);
+n_type = type(n);
+
+print(a_type)
+print(b_type)
+print(c_type)
+print(d_type)
+print(n_type)
