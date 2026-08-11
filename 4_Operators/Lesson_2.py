525a12640572d66db1db531447e8b67d080687ff
Dmitrii2801 <dimaster2804@gmail.com>
2026-08-11 15:17:18 +0500

    New lessons

---
+++ 4_Operators/Lesson_2.py
@@ -0,0 +1,15 @@
+# Обращение к индексам строки
+
+language = 'Python';
+length = len(str(language));
+
+for i in range(length):
+    print(i, '-ый символ: ', language[i], sep='');
+    
+# Обращение по отрицательным индексам - то же самое, что и чтение строки наоборот
+print(language[-1]); # n
+print(language[-2]); # o
+print(language[-3]); # h
+print(language[-4]); # t
+print(language[-5]); # y
+print(language[-6]); # P
