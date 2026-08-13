0a1eb9ffdf2b563009676288c649cd783f7656a8
Dmitrii2801 <dimaster2804@gmail.com>
2026-08-13 18:06:09 +0500

    New lessons

--- 9_Files/Lesson_1.py
+++ 9_Files/Lesson_1.py
@@ -1,15 +1,32 @@
 # Работа с csv-файлами. Чтение данных из файла и обработка этих данных
 
-import pandas as pd # библиотека в том числе для работы с таблицами
+import pandas as pd # библиотека для работы с таблицами (в том числе с файлами .csv)
+import numpy as np # библиотека для математических операций со списками
+import matplotlib.pyplot as plt # библиотека для построения
+# графиков (https://metanit.com/python/matplotlib/), (https://habr.com/ru/articles/1028868/)
 
-file_path = '9_Files\data\est_A.csv'; # путь к файлу
+file_path1 = '9_Files\data\Coil.csv'; # путь к файлу_1
+file_path2 = '9_Files\data\est_A.csv'; # путь к файлу_2
 
-df = pd.read_csv(file_path); # считать данные из csv-файла, расположенного по заданному пути 
+Coil_df = pd.read_csv(file_path1, header=None); # считать данные из csv-файла, расположенного по заданному пути
+est_A_df = pd.read_csv(file_path2, header=None); # считать данные из csv-файла, расположенного по заданному пути
 
-data = df[0].tolist(); # перенести данные из первого столбца (нумерация с 0) в список под названием data
+Coil = Coil_df[0].tolist(); # перенести данные из первого столбца (нумерация с 0) в список под названием Coil
+est_A = est_A_df[0].tolist(); # перенести данные из первого столбца (нумерация с 0) в список под названием est_A
 
-print(data);
+# Временной диапазон для построения графиков
+t0 = 0; # начало записи
+dt = 1e-3; # период дискретизации
+t_stop = dt*len(Coil);
 
+window_size = 50; # длина блочного окна метода матричных пучков
+dt_est = dt*window_size; # период обновления оценок
 
+t = np.arange(t0,t_stop,dt); # временной диапазон для наблюдения
+t_est = np.arange(t0,t_stop-dt_est,dt_est);
 
+# Построение графиков
 
+plt.plot(t, Coil);
+plt.grid()
+plt.show()
