ecc64aa39f9c7f58959f70d0e54db047b8cfe1e5
Dmitrii2801 <dimaster2804@gmail.com>
2026-08-13 16:46:47 +0500

    New lessons

---
+++ 9_Files/Lesson_1.py
@@ -0,0 +1,13 @@
+# Работа с csv-файлами. Чтение данных из файла и обработка этих данных
+
+import csv
+
+with open('data/est_A.csv') as file:
+    for line in file:
+        a = line;
+
+# print(a);
+
+
+
+
