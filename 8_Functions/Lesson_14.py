78ff9fa3ec7c8ccf35b836e73de08dfb1985ea44
Dmitrii2801 <dimaster2804@gmail.com>
2026-08-18 22:16:14 +0500

    New lessons

--- 8_Functions/Lesson_14.py
+++ 8_Functions/Lesson_14.py
@@ -1,31 +1,34 @@
-# Напишите программу, которая содержит внешнюю функцию.
+# Написать программу, которая содержит внешнюю функцию.
 # Функция запрашивает у пользователя два целых числа a и b (гарантируется, что a <= b).
 # Функция должна вывести все простые числа в диапазоне от a до b включительно
 
 import math
 
-def simple_range(left, right):
+# Функция для определения количества простых чисел в диапазоне натуральных чисел от a до b включительно
+def simple_range(left:int, right:int):
     list_simple=[]; # список простых чисел в диапазоне от числа left до числа right включительно
     
-    is_simple=True;
+    # Проверка чисел на простоту
     for number in range(left,right+1):
+        is_simple=True;
         # Числа 0 и 1 не являются простыми
         if number < 2:
             continue;
+        # Проверка делимости числа
         for divider in range(2, math.isqrt(number)+1):
             if(number % divider == 0):
                 is_simple = False;
-                break;
-    if(is_simple == True):
-        list_simple.append(number);
-    return list_simple;
+                # break;
+        if(is_simple == True): # если число простое,
+            list_simple.append(number); # записываем его в список
+    
+    return list_simple; # выходной параметр функции: список простых чисел
 
 # Ввод чисел a и b
 a = int(input('Введите первое натуральное число: '));
 b = int(input('Введите второе натуральное число: '));
 
-simple_list = simple_range(a,b);
-len_simple_list = len(simple_list);
+simple_list = simple_range(a,b); # список простых чисел в диапазоне чисел от a до b включительно
+len_simple_list = len(simple_list); # количество простых чисел
 
 print(f'Количество простых чисел в диапазоне [{a}, {b}] равно {len_simple_list}: {simple_list}');
-
