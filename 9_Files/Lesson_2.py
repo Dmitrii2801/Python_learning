729aa6b1589af9526d5a919fc53f5b16ff647c97
Dmitrii2801 <dimaster2804@gmail.com>
2026-08-23 11:46:03 +0500

    New lessons

---
+++ 9_Files/Lesson_2.py
@@ -0,0 +1,6 @@
+# Продолжение работы с файлами.
+
+# Информация по работе с файлами в Python:
+# 1. https://habr.com/ru/articles/960804/?ysclid=mt5fx9opq9768053815
+# 2. https://habr.com/ru/articles/836696/?ysclid=mt5fxael8f737830544
+# 3. https://habr.com/ru/articles/1005114/?ysclid=mt5fzq7b45179515013
