98528828ee606e823d3ad63deebba15ac63f51e4
Dmitrii2801 <dimaster2804@gmail.com>
2026-09-01 13:07:43 +0500

    New lessons

--- 1_Simple projects/Lesson_10.py
+++ 1_Simple projects/Lesson_10.py
@@ -1,6 +1,15 @@
 # Преобразование строки чисел (каждый элемент имеет тип str) в список чисел (каждый элемент имеет тип float)
 
+# Строка чисел
 str_numbers = '-1 2 3';
-list_numbers = str_numbers.split();
 
-print(f'Полученный список чисел (специально неверный): {list_numbers}');
+# Список чисел, каждое из которых имеет тип str
+list_str_numbers = str_numbers.split();
+
+# Преобразование типа str в тип float
+list_float_numbers = [];
+for i in range(len(list_str_numbers)):
+    list_float_numbers.append(float(list_str_numbers[i]));
+
+print(f'Полученный список чисел типа str: {list_str_numbers}');
+print(f'Полученный список чисел типа float: {list_float_numbers}');
